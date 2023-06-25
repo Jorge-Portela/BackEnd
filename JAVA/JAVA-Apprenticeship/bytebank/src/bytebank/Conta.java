1525d1e9d4190ea99f210a1864a9f0068da97225
Jorge-Portela <jorge.felipe.portela@gmail.com>
2023-06-25 03:12:22 -0300

    Testando novo metodo

--- JAVA/JAVA-Apprenticeship/bytebank/src/bytebank/Conta.java
+++ JAVA/JAVA-Apprenticeship/bytebank/src/bytebank/Conta.java
@@ -1,14 +1,25 @@
 package bytebank;
 
 public class Conta {
-	int saldo;
+	double saldo;
 	int agencia;
 	int numero;
 	String nome;
-	
-	public void deposita(double valor){
-		
-	this.saldo += valor;
-		
+
+	public void deposita(double valor) {
+
+		this.saldo += valor;
+
+	}
+
+	public boolean saca(double valor) {
+
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 }
