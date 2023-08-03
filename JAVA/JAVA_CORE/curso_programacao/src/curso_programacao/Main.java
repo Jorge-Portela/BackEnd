fb7d52713ba337771eccb52ee8913072cccb460f
Jorge-Portela <jorge.felipe.portela@gmail.com>
2023-08-03 03:46:51 -0300

    Add the concept input of data through 'Scanner'

--- JAVA/JAVA_CORE/curso_programacao/src/curso_programacao/Main.java
+++ JAVA/JAVA_CORE/curso_programacao/src/curso_programacao/Main.java
@@ -1,9 +1,36 @@
 package curso_programacao;
 
+import java.util.Locale;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, Wolrd!");
+		double x = 12.5398116544;
+		String name = "João";
+		int age = 31;
+		double income = 4000.0;
+		
+		
+		System.out.println(x);
+		System.out.printf("%.3f%n", x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f\n", x);
+		
+		Locale.setDefault(Locale.US);
+		
+		System.out.printf("%.1f\n",x);
+		System.out.printf("Result %.2f meters \n",x);
+		
+		System.out.println("---------------------");
+		
+	
+		
+		System.out.printf("%s has %d years old and his income's $ %.2f dollars a month.",name,age,income);
+		
+		
+		
+
+		
 	}
 
 }
