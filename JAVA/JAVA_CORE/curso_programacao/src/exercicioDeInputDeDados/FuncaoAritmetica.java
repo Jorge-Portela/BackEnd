9a7d99129f90020d9754e2a6c2b472d06bbf86dc
Jorge-Portela <jorge.felipe.portela@gmail.com>
2023-08-04 23:44:24 -0300

    Adding the variables and method about Arithmetic Expressions

---
+++ JAVA/JAVA_CORE/curso_programacao/src/exercicioDeInputDeDados/FuncaoAritmetica.java
@@ -0,0 +1,32 @@
+package exercicioDeInputDeDados;
+
+public class FuncaoAritmetica {
+	public static void main(String[] args) {
+		
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
+		
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(z);
+		
+		System.out.println("square root of "+x+" = "+A);
+		System.out.println("square root of "+y+" = "+B);
+		System.out.println("square root of 25 = "+C);
+		
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		
+		
+		
+		
+	}
+
+}
