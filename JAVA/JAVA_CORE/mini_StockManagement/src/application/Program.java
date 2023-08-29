eae5e864c1e0cd75c7fe700b48b51b90cffd1643
Jorge-Portela <jorge.felipe.portela@gmail.com>
2023-08-28 23:25:25 -0300

    Ad the methods to remove and add quantity of product

--- JAVA/JAVA_CORE/mini_StockManagement/src/application/Program.java
+++ JAVA/JAVA_CORE/mini_StockManagement/src/application/Program.java
@@ -14,16 +14,32 @@ public class Program {
 		
 		Product product = new Product();
 		System.out.println("Enter product data: ");
-		System.out.println("Name: ");
+		System.out.print("Name: ");
 		product.name = reader.nextLine();
-		System.out.println("Price: ");
+		System.out.print("Price: ");
 		product.price = reader.nextDouble();
-		System.out.println("Quantity in stock: ");
+		System.out.print("Quantity in stock: ");
 		product.quantity = reader.nextInt();
 		
-		System.out.println(product);
+		System.out.println();
+		System.out.println("Product data: " + product);
 		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = reader.nextInt();
+		product.addProducts(quantity);
 		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		quantity = reader.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
+
 		
 		reader.close();
 		
