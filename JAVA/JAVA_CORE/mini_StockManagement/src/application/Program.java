4f0a3553178ca42eb2ac9b91826f4fdc213b1e58
Jorge-Portela <jorge.felipe.portela@gmail.com>
2023-08-28 22:45:22 -0300

    Add the 'toString' method for print the outcome of Program

--- JAVA/JAVA_CORE/mini_StockManagement/src/application/Program.java
+++ JAVA/JAVA_CORE/mini_StockManagement/src/application/Program.java
@@ -13,8 +13,19 @@ public class Program {
 		Scanner reader = new Scanner(System.in);
 		
 		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.println("Name: ");
+		product.name = reader.nextLine();
+		System.out.println("Price: ");
+		product.price = reader.nextDouble();
+		System.out.println("Quantity in stock: ");
+		product.quantity = reader.nextInt();
+		
+		System.out.println(product);
 		
 		
+		
+		reader.close();
 		
 		
 	}
