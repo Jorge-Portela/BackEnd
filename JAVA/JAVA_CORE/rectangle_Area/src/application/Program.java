ab37abf2f522f37e91c4a80fba4021405f4e404f
Jorge-Portela <jorge.felipe.portela@gmail.com>
2023-08-29 23:32:34 -0300

    Add the exercise of rectangle properties with OOP concepts

---
+++ JAVA/JAVA_CORE/rectangle_Area/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		/*
+		 * Fazer um programa para ler os valores da largura e altura 
+		 * de um retângulo. Em seguida, mostrar na tela o valor de 
+		 * sua área, perímetro e diagonal. Usar uma classe como 
+		 * mostrado no projeto ao lado.
+		 */
+		Locale.setDefault(Locale.US);
+		Scanner reader = new Scanner(System.in);
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ JAVA/JAVA_CORE/rectangle_Area/src/entities/AreaRectangle.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class AreaRectangle {
+	/*
+	 * Fazer um programa para ler os valores da largura e altura 
+	 * de um retângulo. Em seguida, mostrar na tela o valor de 
+	 * sua área, perímetro e diagonal. Usar uma classe como 
+	 * mostrado no projeto ao lado.
+	 */
+	
+	public double width;
+	public double height;
+	
+	public double area() {
+		return width * height;
+	}
+	
+	public double perimeter() {
+		return 2*(width + height);
+	}
+	
+	public double diagonal() {
+		return Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+	}
+	
+
+}
