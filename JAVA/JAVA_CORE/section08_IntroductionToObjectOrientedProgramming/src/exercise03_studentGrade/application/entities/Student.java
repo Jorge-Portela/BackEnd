28ddd0e692bd19833de7d60a1413192d383732c3
Jorge-Portela <jorge.felipe.portela@gmail.com>
2023-09-09 02:51:23 -0300

    Refactored the exercise03 by to change the 'CalculateFinalGrade' and 'validateGrade methods'

--- JAVA/JAVA_CORE/section08_IntroductionToObjectOrientedProgramming/src/exercise03_studentGrade/application/entities/Student.java
+++ JAVA/JAVA_CORE/section08_IntroductionToObjectOrientedProgramming/src/exercise03_studentGrade/application/entities/Student.java
@@ -5,24 +5,20 @@ public class Student {
 	public double firstGrade;
 	public double secondGrade;
 	public double thirdGrade;
-	public double finalGrade;
 
-	public Student() {
-	};
-
-	public double calculateFinalGrade(double firstGrade, double secondGrade, double thirdGrade) {
-		return this.finalGrade = this.firstGrade + this.secondGrade + this.thirdGrade;
+	public double calculateFinalGrade() {
+		return this.firstGrade + this.secondGrade + this.thirdGrade;
 	}
 
 	public void validateGrade() {
-		if (calculateFinalGrade(this.firstGrade, this.secondGrade, this.thirdGrade) < 60) {
-			System.out.printf("\nFINAL GRADE = %.2f", this.finalGrade);
+		if (calculateFinalGrade() < 60) {
+			System.out.printf("\nFINAL GRADE = %.2f", calculateFinalGrade());
 			System.out.println("\nFAILED");
-			double missingPointsGrade = 60 - this.finalGrade;
+			double missingPointsGrade = 60 - calculateFinalGrade();
 			System.out.printf("MISSING %.2f POINTS", missingPointsGrade);
 
 		} else {
-			System.out.printf("\n\nFINAL GRADE = %.2f", this.finalGrade);
+			System.out.printf("\n\nFINAL GRADE = %.2f", calculateFinalGrade());
 			System.out.println("\nPASS");
 
 		}
