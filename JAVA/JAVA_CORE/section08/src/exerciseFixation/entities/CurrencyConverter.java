47773554c5a6a94904568c7742ec914dd6ff43d0
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-01-21 03:03:47 -0300

    Added a new resolution of exercise of fixation about static members

---
+++ JAVA/JAVA_CORE/section08/src/exerciseFixation/entities/CurrencyConverter.java
@@ -0,0 +1,18 @@
+package exerciseFixation.entities;
+
+public class CurrencyConverter {
+
+    public static final double IOF = 0.06;
+
+
+    public static double convetorDollar(double dollarPrice, double dollarBought){
+        
+        return dollarPrice * dollarBought * (1 +  IOF);
+    }
+
+
+
+
+
+
+}
