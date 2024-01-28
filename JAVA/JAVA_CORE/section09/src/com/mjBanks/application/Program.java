c775a229109fa24a827bf773decf06f259a1117a
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-01-28 01:50:10 -0300

    Created the first body of project

--- JAVA/JAVA_CORE/section09/src/com/mjBanks/application/Program.java
+++ JAVA/JAVA_CORE/section09/src/com/mjBanks/application/Program.java
@@ -1,5 +1,38 @@
 package com.mjBanks.application;
 
+import com.mjBanks.entities.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] arg){
+
+        Locale.setDefault(Locale.US);
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("MJ BANK");
+
+        System.out.print("Enter account number:");
+        int accountNumber = reader.nextInt();
+
+        System.out.print("\nEnter account holder:");
+        String holder = reader.nextLine();
+
+        System.out.print("\nIs there an initial deposit (y/n): ");
+        String responseInitialDeposit = reader.nextLine();
+
+
+        System.out.print("\nEnter initial deposit value: ");
+        double initialDeposit = reader.nextDouble();
+
+        Account account1 = new Account(accountNumber,holder,initialDeposit);
+
+
+
+        reader.close();
+
+    }
 
 }
--- JAVA/JAVA_CORE/section09/src/com/mjBanks/entities/Account.java
+++ JAVA/JAVA_CORE/section09/src/com/mjBanks/entities/Account.java
@@ -4,13 +4,38 @@ public class Account {
     private int accountNumber;
     private String holder;
     private  double initialDeposit;
+    private double balance;
 
-    Account(int accountNumber, String holder, double initialDeposit){
+    public Account(int accountNumber, String holder, double initialDeposit){
         this.accountNumber = accountNumber;
         this.holder = holder;
+
     }
-    
 
+    //Enter the Getters and Setters
 
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    //No Setter was created for the accountNumber attribute;
+    //According to the project's business rules, it is not allowed to change the account number
+
+    public String getHolder(){
+        return this.holder;
+    }
+
+    public void setHolder(String holder){
+        this.holder = holder;
+    }
+
+    public void isDeposit(double deposit){
+        balance += deposit;
+    }
+
+    //There is a tax of $5.00 over each withdraw(Business Rules)
+    public double isWithdraw(double withdraw){
+        return this.balance -= (withdraw + 5.00);
+    }
 
 }
