fa3579d28711d56953d4d143c6ecd770f49a79da
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-01-27 23:41:17 -0300

    Added ´ṕrivate'on attibutes of Account class

--- JAVA/JAVA_CORE/section09/src/com/mjBanks/entities/Account.java
+++ JAVA/JAVA_CORE/section09/src/com/mjBanks/entities/Account.java
@@ -1,13 +1,16 @@
 package com.mjBanks.entities;
 
 public class Account {
-    int accountNumber;
-    String holder;
-    double initialDeposit;
+    private int accountNumber;
+    private String holder;
+    private  double initialDeposit;
 
     Account(int accountNumber, String holder, double initialDeposit){
         this.accountNumber = accountNumber;
         this.holder = holder;
-
     }
+    
+
+
+
 }
