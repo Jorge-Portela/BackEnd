7509d00b30145224afdebf1f31183b01da52f4a0
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-01-30 23:04:35 -0300

    Deleted the Class Scanner 'readerDouble' and created a Scanner 'readerNumb' that receives the type double  and int inputs

--- JAVA/JAVA_CORE/section10_ArraysAndLists/src/examples/example02/application/Program.java
+++ JAVA/JAVA_CORE/section10_ArraysAndLists/src/examples/example02/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner readerNumb = new Scanner(System.in);
         Scanner readerString = new Scanner(System.in);
-        //Scanner readerDouble = new Scanner(System.in);
+        
 
         System.out.print("\nEnter the quantity of product on the inputs: ");
         int n = readerNumb.nextInt();
@@ -52,6 +52,5 @@ public class Program {
 
         readerNumb.close();
         readerString.close();
-        //readerDouble.close();
     }
 }
