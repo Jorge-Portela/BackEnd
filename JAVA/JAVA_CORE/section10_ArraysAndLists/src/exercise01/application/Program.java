002014c396241cdaf06fd37c208ceb9644ec106f
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-02-01 23:39:38 -0300

    Added the exercise01 title with example of requiremnts

---
+++ JAVA/JAVA_CORE/section10_ArraysAndLists/src/exercise01/application/Program.java
@@ -0,0 +1,36 @@
+package exercise01.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+*                  Problema "negativos"
+* Faça um programa que leia um número inteiero positivo N (Máximo = 10) e
+*  depois N números inteiros e armazene-os em um vetor. Em seguida,
+* mostra na tela todos os números negativos lidos.
+*
+* Exemplo:
+* Quantos numeros você vai digitar: 6
+* Digite um número: 8
+* Digite um número: -2
+* Digite um número: 9
+* Digite um número: 10
+* Digite um número: -3
+* Digite um número: -7
+* NUMEROS NEGATIVOS:
+* -2
+* -3
+* -7
+* */
+
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner reader = new Scanner(System.in);
+
+
+
+        reader.close();
+    }
+
+}
