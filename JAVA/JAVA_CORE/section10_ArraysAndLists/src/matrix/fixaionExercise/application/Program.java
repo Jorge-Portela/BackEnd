7193aed4dd124bc16ae4ac687e7962cbf0830501
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-02-23 01:30:51 -0300

    Added struct of fixation exercise about matrix

---
+++ JAVA/JAVA_CORE/section10_ArraysAndLists/src/matrix/fixaionExercise/application/Program.java
@@ -0,0 +1,15 @@
+package matrix.fixaionExercise.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner reader = new Scanner(System.in);
+
+
+
+        
+        reader.close();
+    }
+}
