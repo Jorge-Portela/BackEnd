e625f3b3b78f507f5ed80675ff5aebed72cd3ec9
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-02-24 18:18:35 -0300

    Added the example of Local Date methods with comment about the outputs

--- JAVA/JAVA_CORE/section11_DateHour/src/exampleExercise/Application/Program.java
+++ JAVA/JAVA_CORE/section11_DateHour/src/exampleExercise/Application/Program.java
@@ -6,36 +6,52 @@ import java.time.format.DateTimeFormatter;
 
 public class Program {
     public static void main(String[] args){
-        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter fmt2 =  DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-        LocalDate d01 = LocalDate.now();
-        LocalDateTime d02 = LocalDateTime.now();
-        Instant d03 = Instant.now();
-
-        LocalDate d04 = LocalDate.parse("2022-07-20");
-        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
-        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
-        Instant d07 = Instant.parse("2022-07-20T01:30:26-03:00");
 
         // https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/format/DateTimeFormatter.html
-        LocalDate d08 = LocalDate.parse("20/07/2022", fmt1);
-        LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
 
-        LocalDate d10 = LocalDate.of(2022, 07, 20);
-        LocalDateTime d11 = LocalDateTime.of(2022, 07, 20, 1, 30);
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        System.out.println("d01 = " + d01.toString());
-        System.out.println("d02 = " + d02.toString());
-        System.out.println("d03 = " + d03.toString());
-        System.out.println("d04 = " + d04.toString());
-        System.out.println("d05 = " + d05.toString());
-        System.out.println("d06 = " + d06.toString());
-        System.out.println("d07 = " + d07.toString());
-        System.out.println("d08 = " + d08.toString());
-        System.out.println("d09 = " + d09.toString());
-        System.out.println("d10 = " + d10.toString());
-        System.out.println("d11 = " + d11.toString());
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
+        //Reports the local date at this moment
+        LocalDate d01 = LocalDate.now();
+        //Reports the local date with time at this moment
+        LocalDateTime d02 = LocalDateTime.now();
+        //Reports the Global Date at this moment
+        Instant d03 = Instant.now();
+
+        //Report date through input of String format
+        LocalDate d04 = LocalDate.parse("2024-02-24");
+        //Report date and time through input of String format
+        LocalDateTime d05 = LocalDateTime.parse("2024-02-24T17:30:20");
+        //Report Global date and time through input of String format
+        Instant d06 = Instant.parse("2024-02-24T17:30:20Z");
+        //Report Global date and time through input of Local time on String format
+        Instant d07 = Instant.parse("2024-02-24T17:30:20-03:00");
+
+        //Report Local Date with parameter to format the output entered by String
+        LocalDate d08 = LocalDate.parse("24/02/2024",fmt1);
+
+        //Report Local Date Time with parameter to format the output entered by String
+        LocalDateTime d09 = LocalDateTime.parse("24/02/2024 18:07",fmt2);
+
+        //Report Date entered by parameter
+        LocalDate d10 = LocalDate.of(2024, 02, 24);
+
+        //Report Date Time entered by parameter
+        LocalDateTime d11 = LocalDateTime.of(2024, 02, 24, 18, 15, 20);
+
+        System.out.println("d01 = " + d01);
+        System.out.println("d02 = " + d02);
+        System.out.println("d03 = " + d03);
+        System.out.println("d04 = " + d04);
+        System.out.println("d05 = " + d05);
+        System.out.println("d06 = " + d06);
+        System.out.println("d07 = " + d07);
+        System.out.println("d08 = " + d08);
+        System.out.println("d09 = " + d09);
+        System.out.println("d10 = " + d10);
+        System.out.println("d11 = " + d11);
     }
 }
