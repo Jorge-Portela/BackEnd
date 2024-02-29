7f5a24863138125875e3bbb2874056d18e22888f
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-02-28 22:50:39 -0300

    Adjust the getters and setter method orders of 'HourContract' Class

--- JAVA/JAVA_CORE/section13_EnumAndComposition/src/workContract/entities/HourContract.java
+++ JAVA/JAVA_CORE/section13_EnumAndComposition/src/workContract/entities/HourContract.java
@@ -15,6 +15,30 @@ public class HourContract {
         this.hours = hours;
     }
 
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour) {
+        this.valuePerHour = valuePerHour;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
     public Double totalValue(){
         return this.valuePerHour * hours;
     }
