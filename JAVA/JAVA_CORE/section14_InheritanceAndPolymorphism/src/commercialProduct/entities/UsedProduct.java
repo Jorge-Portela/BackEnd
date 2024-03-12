6b0abf7c73a062acbfc4043c2a75806dfcb756e5
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-03-12 04:19:40 -0300

    Added initial blueprint of 'Commercial Product' mini project

---
+++ JAVA/JAVA_CORE/section14_InheritanceAndPolymorphism/src/commercialProduct/entities/UsedProduct.java
@@ -0,0 +1,21 @@
+package commercialProduct.entities;
+
+import java.time.LocalDate;
+
+public class UsedProduct extends Product {
+    private LocalDate manufactureDate;
+
+    public UsedProduct(){}
+    public UsedProduct(String name, Double price, LocalDate manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public LocalDate getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(LocalDate manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+}
