65cbddaa52b2dd4476ea418d1fcb6990acf66f78
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-03-15 03:18:43 -0300

    Added the 'Shape' abstract generic super class, 'Rectangle' and 'Circle' sub classes and 'Color' enum

--- JAVA/JAVA_CORE/section14_InheritanceAndPolymorphism/src/geometricShapes/entities/Shape.java
+++ JAVA/JAVA_CORE/section14_InheritanceAndPolymorphism/src/geometricShapes/entities/Shape.java
@@ -1,4 +1,24 @@
 package geometricShapes.entities;
 
-public class Shape {
+import geometricShapes.entities.enums.Color;
+
+public abstract class Shape {
+    private Color color;
+
+    public Shape(){}
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract Double area();
+
 }
