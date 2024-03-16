f09c3da8a4f927599c14653c4b1995dd92bdce65
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-03-16 01:30:38 -0300

    Added the constructors and methods of 'Company' Class

--- JAVA/JAVA_CORE/section14_InheritanceAndPolymorphism/src/taxManagement/entities/Company.java
+++ JAVA/JAVA_CORE/section14_InheritanceAndPolymorphism/src/taxManagement/entities/Company.java
@@ -1,5 +1,29 @@
 package taxManagement.entities;
 
-public class Company extends TaxPayer{
-    
+public class Company extends TaxPayer {
+    private int numberOfEmployees;
+
+    public Company(String name, Double annualIncome, int numberOfEmployees) {
+        super(name, annualIncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    @Override
+    public Double tax() {
+        double taxCompany = 0.0;
+        if (this.numberOfEmployees > 10) {
+            taxCompany = this.getAnnualIncome() * 0.14;
+        } else {
+            taxCompany = this.getAnnualIncome() * 0.16;
+        }
+        return taxCompany;
+    }
 }
