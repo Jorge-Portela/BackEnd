14063ca3509ce722c9c944b3f75871dd1d7b8ca8
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-03-18 02:41:23 -0300

    Added examplo of satck trace in 'Exception' concept by using 'printStackTrace' method and using 'split' method too

--- JAVA/JAVA_CORE/section15_ExceptionTreatment/src/example/application/Program.java
+++ JAVA/JAVA_CORE/section15_ExceptionTreatment/src/example/application/Program.java
@@ -7,8 +7,24 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner reader = new Scanner(System.in);
 
+        method1();
+
+        System.out.println("End of Program.");
+
+
+    }
+
+    public static void method1(){
+        System.out.println("***METHOD 1 START  ***");
+
+        method2();
+
+        System.out.println("***METHOD 1 START  ***");
+    }
+    public static void method2(){
+        System.out.println("***METHOD 2 START***");
+        Scanner reader = new Scanner(System.in);
         try{
             String [] vector = reader.nextLine().split(" ");
             int position = reader.nextInt();
@@ -16,13 +32,14 @@ public class Program {
 
         } catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Invalid position entered!");
+            e.printStackTrace();
+            reader.next();
         } catch(InputMismatchException e ){
             System.out.println("Invalid type entered");
+            e.printStackTrace();
         }
 
-        System.out.println("End of Program.");
-
-
+        System.out.println("***METHOD 2 END***");
 
         reader.close();
     }
