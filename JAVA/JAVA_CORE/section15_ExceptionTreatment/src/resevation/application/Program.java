08371a387d7c3026c0f4a3d29e0d6836bad6013c
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-03-27 21:13:58 -0300

    Added 'try-catch of IllegalArgumentException'

--- JAVA/JAVA_CORE/section15_ExceptionTreatment/src/resevation/application/Program.java
+++ JAVA/JAVA_CORE/section15_ExceptionTreatment/src/resevation/application/Program.java
@@ -13,29 +13,34 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner reader = new Scanner(System.in);
 
+
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        try{
+            System.out.print("Room number: ");
+            int numbRoom = reader.nextInt();
+            System.out.print("Check-In date (dd/MM/yyyy): ");
+            LocalDate checkIn = LocalDate.parse(reader.next(), fmt);
+            System.out.print("Check-Out date (dd/MM/yyyy): ");
+            LocalDate checkOut = LocalDate.parse(reader.next(), fmt);
 
-        System.out.print("Room number: ");
-        int numbRoom = reader.nextInt();
-        System.out.print("Check-In date (dd/MM/yyyy): ");
-        LocalDate checkIn = LocalDate.parse(reader.next(), fmt);
-        System.out.print("Check-Out date (dd/MM/yyyy): ");
-        LocalDate checkOut = LocalDate.parse(reader.next(), fmt);
-
-        Reservation reservation = new Reservation(numbRoom, checkIn, checkOut);
-        System.out.println(reservation);
+            Reservation reservation = new Reservation(numbRoom, checkIn, checkOut);
+            System.out.println(reservation);
 
 
-        System.out.println("\nEnter data to update the reservation: ");
-        System.out.print("Check-In date (dd/MM/yyyy): ");
-        LocalDate updatedCheckIn = LocalDate.parse(reader.next(), fmt);
-        System.out.print("Check-Out date (dd/MM/yyyy): ");
-        LocalDate updatedCheckOut = LocalDate.parse(reader.next(), fmt);
+            System.out.println("\nEnter data to update the reservation: ");
+            System.out.print("Check-In date (dd/MM/yyyy): ");
+            LocalDate updatedCheckIn = LocalDate.parse(reader.next(), fmt);
+            System.out.print("Check-Out date (dd/MM/yyyy): ");
+            LocalDate updatedCheckOut = LocalDate.parse(reader.next(), fmt);
 
-        reservation.updateDates(updatedCheckIn, updatedCheckOut);
+            reservation.updateDates(updatedCheckIn, updatedCheckOut);
 
-        System.out.println(reservation);
-      
+            System.out.println(reservation);
+
+
+        } catch(IllegalArgumentException e){
+            System.out.println("Error in reservation: "+e.getMessage());
+        }
 
         reader.close();
     }
--- JAVA/JAVA_CORE/section15_ExceptionTreatment/src/resevation/model/entities/Reservation.java
+++ JAVA/JAVA_CORE/section15_ExceptionTreatment/src/resevation/model/entities/Reservation.java
@@ -39,15 +39,14 @@ public class Reservation {
         return ChronoUnit.DAYS.between(getCheckIn(), getCheckOut());
     }
 
-    public  String updateDates(LocalDate updatedCheckIn, LocalDate updatedCheckOut) {
+    public  void updateDates(LocalDate updatedCheckIn, LocalDate updatedCheckOut) {
         if(!updatedCheckIn.isBefore(updatedCheckOut) || updatedCheckOut.isBefore(LocalDate.now())){
-             return "Reservation dates for updates must be future dates";
+             throw new IllegalArgumentException("Reservation dates for updates must be future dates");
         } if(!updatedCheckOut.isAfter(updatedCheckIn)){
-            return "Reservation dates Check-Out date must be after Check-In";
+            throw new IllegalArgumentException("Reservation dates Check-Out date must be after Check-In");
         }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        return null;
     }
 
     public String toString() {
