c220746ad8cfa22af48dba87b5eeaed99e8ebf50
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-04 18:50:39 -0300

    Added catch 'RuntimeException' type for treat generics errors

--- JAVA/JAVA_CORE/section15_ExceptionTreatment/src/resevation/application/Program.java
+++ JAVA/JAVA_CORE/section15_ExceptionTreatment/src/resevation/application/Program.java
@@ -41,6 +41,8 @@ public class Program {
 
         } catch(DomainException e) {
             System.out.println("Error in reservation: " + e.getMessage());
+        } catch (RuntimeException e){
+            System.out.println("Unexpected error");
         }
 
         reader.close();
