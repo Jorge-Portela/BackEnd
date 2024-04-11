f0d5263c6ad2fabf0b103292ca2d21832bd5cbed
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-11 19:41:31 -0300

    Added example of File reader by 'File' class

--- JAVA/JAVA_CORE/section17_WorkingWithFiles/src/workingwithfiles/application/Program.java
+++ JAVA/JAVA_CORE/section17_WorkingWithFiles/src/workingwithfiles/application/Program.java
@@ -1,30 +1,29 @@
-package example2.application;
+package workingwithfiles.application;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
+import java.io.File;
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
-        File file = new File("/home/jorge-portela/Documents/test_exeption/exemplo.txt");
-
-
+        File file = new File("/home/jorge-portela/Documents/Cursos/JAVA _Nelio/chap_17/example.txt");
         Scanner reader = null;
-
         try {
             reader = new Scanner(file);
             while (reader.hasNextLine()) {
                 System.out.println(reader.nextLine());
             }
         } catch (IOException e) {
-            System.out.println("Error opening file: " + e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         } finally {
             if (reader != null) {
                 reader.close();
             }
         }
+
+
     }
 }
