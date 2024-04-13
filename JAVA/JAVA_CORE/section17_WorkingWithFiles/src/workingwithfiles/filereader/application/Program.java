be58749584d42317490d20ceb60fc619a77379cf
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-12 22:07:51 -0300

    Added scope of mini project to reader csv files

---
+++ JAVA/JAVA_CORE/section17_WorkingWithFiles/src/workingwithfiles/filereader/application/Program.java
@@ -0,0 +1,11 @@
+package workingwithfiles.filereader.application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        reader.close();
+    }
+}
