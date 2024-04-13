79a41813cf7c3f79c4879044312a2f0a8f5318af
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-13 11:49:18 -0300

    Added first scope of create file csv mini project

--- JAVA/JAVA_CORE/section17_WorkingWithFiles/src/workingwithfiles/example2/application/Program.java
+++ JAVA/JAVA_CORE/section17_WorkingWithFiles/src/workingwithfiles/example2/application/Program.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Program {
     public static void main(String[] args) {
         String path = "path";
-        
+
 
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
@@ -15,14 +15,6 @@ public class Program {
             while (line != null) {
                 System.out.println(line);
                 line = br.readLine();
-
-
-
-
-
-
-
-
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
--- JAVA/JAVA_CORE/section17_WorkingWithFiles/src/workingwithfiles/filereader/application/Program.java
+++ JAVA/JAVA_CORE/section17_WorkingWithFiles/src/workingwithfiles/filereader/application/Program.java
@@ -1,10 +1,25 @@
 package workingwithfiles.filereader.application;
 
+import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner reader = new Scanner(System.in);
+
+        File file = new File("path/to/file.txt");
+
+        System.out.println("Enter Product Name:");
+        String productName = reader.nextLine();
+        System.out.println("Enter product price:");
+        double productPrice = reader.nextDouble();
+        System.out.println("Enter product amount:");
+        int productAmount = reader.nextInt();
+
+
+
 
         reader.close();
     }
