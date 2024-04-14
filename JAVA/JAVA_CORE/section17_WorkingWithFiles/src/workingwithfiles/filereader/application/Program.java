c1d46a19ae7754d2f1840c694b826051228588b5
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-14 18:15:30 -0300

    Added 'Profuct' Class from 'File Reader' mini project

--- JAVA/JAVA_CORE/section17_WorkingWithFiles/src/workingwithfiles/filereader/application/Program.java
+++ JAVA/JAVA_CORE/section17_WorkingWithFiles/src/workingwithfiles/filereader/application/Program.java
@@ -9,14 +9,26 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner reader = new Scanner(System.in);
 
-        File file = new File("path/to/file.txt");
+        File file = new File("/home/jorge-portela/Documents/create_file_project/product.csv");
 
-        System.out.println("Enter Product Name:");
-        String productName = reader.nextLine();
-        System.out.println("Enter product price:");
-        double productPrice = reader.nextDouble();
-        System.out.println("Enter product amount:");
-        int productAmount = reader.nextInt();
+        System.out.println("How much products will be entered? ");
+        int numbInput = reader.nextInt();
+        reader.nextLine();
+
+        String[][] products = new String[numbInput][numbInput];
+
+        for(int i = 1; i <= numbInput; i++){
+            for (int j = 1; j<=numbInput; j++){
+                System.out.println("Enter Product Name:");
+                String productName = reader.nextLine();
+                System.out.println("Enter product price:");
+                double productPrice = reader.nextDouble();
+                System.out.println("Enter product amount:");
+                int productAmount = reader.nextInt();
+
+            }
+        }
+
 
 
 
