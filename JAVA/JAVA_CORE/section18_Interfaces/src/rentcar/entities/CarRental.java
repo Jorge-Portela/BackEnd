3649639426a2916b075beda3138da762f2ee345f
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-16 15:25:22 -0300

    Added default 'empty' constructor of 'Car Rental' Class from 'Car Rental' mini project

--- JAVA/JAVA_CORE/section18_Interfaces/src/rentcar/entities/CarRental.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/rentcar/entities/CarRental.java
@@ -6,6 +6,8 @@ public class CarRental {
     private LocalDateTime start;
     private LocalDateTime finish;
 
+    public CarRental() {}
+    
     public CarRental(LocalDateTime start, LocalDateTime finish) {
         this.start = start;
         this.finish = finish;
