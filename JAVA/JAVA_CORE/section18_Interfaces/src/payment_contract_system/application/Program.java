4f7789458a0779d01f005bbbf42af2af1e0c9307
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-19 00:20:40 -0300

    Added 'Program' Class to application of mini project

---
+++ JAVA/JAVA_CORE/section18_Interfaces/src/payment_contract_system/application/Program.java
@@ -0,0 +1,15 @@
+package payment_contract_system.application;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class Program {
+    public static void main (String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner reader = new Scanner(System.in);
+
+        reader.close();
+
+    }
+}
