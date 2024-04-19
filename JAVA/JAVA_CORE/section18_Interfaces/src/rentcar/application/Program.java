a250c4b1f1329e9fa1b7079dc771fef4a5912d14
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-18 23:14:42 -0300

    Added output prints message on 'Program' class to execute

--- JAVA/JAVA_CORE/section18_Interfaces/src/rentcar/application/Program.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/rentcar/application/Program.java
@@ -18,21 +18,30 @@ public class Program {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         System.out.println("Enter the rentals data: ");
-        System.out.println("Car Model: ");
+        System.out.print("Car Model: ");
         String carModel = reader.nextLine();
-        System.out.println("Pick up (dd/MM/yyyy hh:mm): ");
-        LocalDateTime start = LocalDateTime.parse(reader.nextLine(),fmt);
-        System.out.println("Return (dd/MM/yyyy hh:mm): ");
-        LocalDateTime finish = LocalDateTime.parse(reader.nextLine(),fmt);
+        System.out.print("Pick up (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(reader.nextLine(), fmt);
+        System.out.print("Return (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(reader.nextLine(), fmt);
 
-        CarRental carRental = new CarRental(start,finish,new Vehicle(carModel));
+        CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
 
         System.out.print("Enter the price per hour: ");
         double pricePerHour = reader.nextDouble();
-        System.out.println("Enter the price per day: ");
+        System.out.print("Enter the price per day: ");
         double pricePerDay = reader.nextDouble();
 
-        RentalService rentalService = new RentalService(pricePerHour,pricePerDay,new BrazilTaxService());
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(carRental);
+
+        System.out.println("\n========================================\n");
+
+        System.out.println("INVOICE: ");
+        System.out.println("Basic Payment: " + String.format("%.2f", carRental.getInvoice().getBasicPayment()));
+        System.out.println("Taxes: " + String.format("%.2f", carRental.getInvoice().getTax()));
+        System.out.println("Total Payment: " + String.format("%.2f", carRental.getInvoice().getTotalPayment()));
 
         reader.close();
     }
