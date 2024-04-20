dbaaa1830fa91ee51dc6ec8b491d6b2da846d1b0
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-19 23:36:10 -0300

    Added 'Program' inputs from application layer from mini project

--- JAVA/JAVA_CORE/section18_Interfaces/src/payment_contract_system/application/Program.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/payment_contract_system/application/Program.java
@@ -1,5 +1,9 @@
 package payment_contract_system.application;
 
+import payment_contract_system.entities.Contract;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import java.util.Locale;
 
@@ -8,6 +12,22 @@ public class Program {
     public static void main (String[] args){
         Locale.setDefault(Locale.US);
         Scanner reader = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Enter contract data");
+        System.out.print("Number: ");
+        int number = reader.nextInt();
+        System.out.print("Date (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(reader.next(),fmt);
+        System.out.print("Contract value: ");
+        double contractValue = reader.nextDouble();
+        System.out.print("Enter number of installments: ");
+        int installments = reader.nextInt();
+
+        Contract contract = new Contract(number, date, contractValue);
+
+
 
         reader.close();
 
--- JAVA/JAVA_CORE/section18_Interfaces/src/payment_contract_system/model/services/ContractService.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/payment_contract_system/model/services/ContractService.java
@@ -3,5 +3,7 @@ package payment_contract_system.model.services;
 import payment_contract_system.entities.Contract;
 
 public class ContractService {
+
     public void processContract(Contract contract, Integer months){}
+
 }
