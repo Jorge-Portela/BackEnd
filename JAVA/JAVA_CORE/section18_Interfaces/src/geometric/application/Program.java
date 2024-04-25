315a817798684765afb86cf3f758dc1813ef3256
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-24 22:10:42 -0300

    Added 'Program' class to execute the aplplication of 'geometric' mini project

--- JAVA/JAVA_CORE/section18_Interfaces/src/geometric/application/Program.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/geometric/application/Program.java
@@ -1,5 +1,10 @@
 package geometric.application;
 
+import geometric.model.entities.Circle;
+import geometric.model.entities.Rectangle;
+import geometric.model.entities.Shape;
+import geometric.model.enums.Color;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,6 +12,14 @@ public class Program {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner reader = new Scanner(System.in);
+
+        Shape s1 = new Circle(Color.BLACK, 2.0);
+        Shape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+
+        System.out.println("Circle color: " + s1.getColor());
+        System.out.println("Circle area: " + String.format("%.3f", s1.area()));
+        System.out.println("Rectangle color: " + s2.getColor());
+        System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
         
         reader.close();
 
