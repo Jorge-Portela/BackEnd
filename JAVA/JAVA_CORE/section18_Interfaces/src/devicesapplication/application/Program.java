995a4226e67d25c4a21a6d26e70e21ca31a39396
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-28 06:31:07 -0300

    Added 'Program' class to execute the application

--- JAVA/JAVA_CORE/section18_Interfaces/src/devicesapplication/application/Program.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/devicesapplication/application/Program.java
@@ -1,4 +1,14 @@
 package devicesapplication.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner reader = new Scanner(System.in);
+
+        reader.close();
+
+    }
 }
