b9d7dab8d9558640e43fb402a0ab68d56448df57
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-04-30 04:38:05 -0300

    Added 'Program' class to read a file that contain list of employers names

--- JAVA/JAVA_CORE/section18_Interfaces/src/employeenames/application/Program.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/employeenames/application/Program.java
@@ -1,4 +1,30 @@
 package employeenames.application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class Program {
+    public static void main(String[] args){
+
+        List<String> list = new ArrayList<>();
+        String path = "";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+
+            String name = br.readLine();
+            while(name != null){
+                list.add(name);
+                name = br.readLine();
+            }
+            Collections.sort(list);
+            for (String s : list){
+                System.out.println(s);
+            }
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
 }
