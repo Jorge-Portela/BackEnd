f5bf5d32915e74851d98e781d4639d2ef534461b
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-03 00:37:14 -0300

    Added soma applications of Object 'pen1' tah is instaced of 'Pen' class

--- JAVA/OOP_Concepts/chapter01_ClassesAndObjects/src/Program.java
+++ JAVA/OOP_Concepts/chapter01_ClassesAndObjects/src/Program.java
@@ -2,9 +2,18 @@ public class Program {
     public static void main(String[] args){
 
         Pen pen1 = new Pen();
+        pen1.model = "Bic Spherical";
         pen1.color = "Black";
         pen1.tip = 0.50;
+        pen1.filler = 15;
         pen1.cover = false;
+
+        pen1.isScribble();
+
+        System.out.println("The Pen is covered ? True = Yes , False = No. Answer: "+pen1.getCover());
+
+        System.out.println("The Pen need to be Uncovered ? True = Yes , False = No. Answer: "+pen1.getUncover());
+
 
 
     }
