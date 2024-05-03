4d522427375fa08fb8f3f48e5e2f04471ee93e67
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-02 22:56:48 -0300

    Added the 'Pen' class and its methods of chapter 01 about 'Classes and Objestc' concepts

---
+++ JAVA/OOP_Concepts/chapter01_ClassesAndObjects/src/Pen.java
@@ -0,0 +1,24 @@
+public class Pen {
+
+    String model;
+    String color;
+    Double tip;
+    int filler;
+    boolean cover;
+
+    void isScribble(){
+        if(cover){
+            System.out.println("ERROR");
+        }else{
+            System.out.println("Scribble");
+        }
+    }
+
+    boolean getCover(){
+        if(cover){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
