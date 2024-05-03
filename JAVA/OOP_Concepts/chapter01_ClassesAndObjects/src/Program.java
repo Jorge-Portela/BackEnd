b4af81dfc3357348b5d69c230d673f5f3cc61925
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-02 23:16:04 -0300

    Added intanciate of 'Pen' Class and some attributes and fileds

--- JAVA/OOP_Concepts/chapter01_ClassesAndObjects/src/Program.java
+++ JAVA/OOP_Concepts/chapter01_ClassesAndObjects/src/Program.java
@@ -1,5 +1,11 @@
 public class Program {
     public static void main(String[] args){
-        
+
+        Pen pen1 = new Pen();
+        pen1.color = "Black";
+        pen1.tip = 0.50;
+        pen1.cover = false;
+
+
     }
 }
