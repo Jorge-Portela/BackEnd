f54ea7f523aab53d9deab063710aff4ae971c5e0
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-03 23:52:37 -0300

    Added 'status' method to 'Pen' class

--- JAVA/OOP_Concepts/chapter01_ClassesAndObjects/src/Pen.java
+++ JAVA/OOP_Concepts/chapter01_ClassesAndObjects/src/Pen.java
@@ -6,6 +6,14 @@ public class Pen {
     int filler;
     boolean cover;
 
+    void status(){
+        System.out.println("Pen model: " + this.model);
+        System.out.println("Pen color: " + this.color);
+        System.out.println("Pen tip: " + this.tip);
+        System.out.println("Pen filler: " + this.filler);
+        System.out.println("Pen cover: " + this.cover);
+    }
+
     void isScribble(){
         if(cover){
             System.out.println("ERROR");
