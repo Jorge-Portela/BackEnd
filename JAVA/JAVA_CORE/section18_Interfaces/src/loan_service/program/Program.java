9950aca1174ba816ec43d5e9799b7b60909eeda9
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-05 01:54:18 -0300

    Added 'BrazilInterestService' and 'UsaInterestService' classes in 'Program' Class to run the aplication with changes of 'InterestService' methods implemented

--- JAVA/JAVA_CORE/section18_Interfaces/src/loan_service/program/Program.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/loan_service/program/Program.java
@@ -2,6 +2,7 @@ package loan_service.program;
 
 
 import loan_service.entities.BrazilInterestService;
+import loan_service.entities.InterestService;
 import loan_service.entities.UsaInterestService;
 
 import java.util.Locale;
@@ -17,11 +18,23 @@ public class Program {
         System.out.print("Months: ");
         int month = reader.nextInt();
 
-        UsaInterestService usaist = new UsaInterestService(1.0);
-        double payment = usaist.payment(amount, month);
+        System.out.println("\n=========================\n");
 
+        InterestService usaist = new UsaInterestService(1.0);
+        double paymentUsaist = usaist.payment(amount, month);
+
+        System.out.println("USA Interest Rate: 1%");
         System.out.println("Payment after " + month + " months:");
-        System.out.println(String.format("R$ %.2f",payment));
+        System.out.println(String.format("TOTAL: US $ %.2f",paymentUsaist));
+
+        System.out.println("\n---------------------------\n");
+
+        InterestService brist = new BrazilInterestService(2.0);
+        double paymentBrist = brist.payment(amount, month);
+
+        System.out.println("Brazilian Interest Rate: 2%");
+        System.out.println("Payment after " + month + " months:");
+        System.out.println(String.format("TOTAL: R$ %.2f",paymentBrist));
 
         reader.close();
 
