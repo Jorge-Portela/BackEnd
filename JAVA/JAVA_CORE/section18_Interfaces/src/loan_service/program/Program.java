374f9a34b59674a67bf9a26ebef0ea211d7be66b
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-04 23:56:56 -0300

    Added inital interation of input data of mini project

--- JAVA/JAVA_CORE/section18_Interfaces/src/loan_service/program/Program.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/loan_service/program/Program.java
@@ -1,5 +1,6 @@
 package loan_service.program;
 
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,7 +8,14 @@ public class Program {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner reader = new Scanner(System.in);
+
+        System.out.print("Amount: ");
+        double amount = reader.nextDouble();
+        System.out.println("Months: ");
+        int months = reader.nextInt();
+
         
+
         reader.close();
 
     }
