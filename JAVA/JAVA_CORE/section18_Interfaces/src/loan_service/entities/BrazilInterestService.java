a5f6d5fb3a094a2e38b1e14f9fe285203acb30d2
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-05 01:34:46 -0300

    Implement 'InterestService' interface to 'BrazilInterestService' class

--- JAVA/JAVA_CORE/section18_Interfaces/src/loan_service/entities/BrazilInterestService.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/loan_service/entities/BrazilInterestService.java
@@ -2,22 +2,17 @@ package loan_service.entities;
 
 import java.security.InvalidParameterException;
 
-public class BrazilInterestService {
+public class BrazilInterestService implements InterestService{
     private double interestRate;
 
     public BrazilInterestService(double interestRate){
         this.interestRate = interestRate;
     }
 
+    @Override
     public double getInterestRate(){
         return interestRate;
     }
 
-    public double payment(double amount, int month){
-        if(month < 1){
-            throw new InvalidParameterException("Month must be greater than '0' zero  ");
-        }
-        return amount * Math.pow(1.0 + interestRate / 100.0, month);
-    }
 
 }
--- JAVA/JAVA_CORE/section18_Interfaces/src/loan_service/entities/InterestService.java
+++ JAVA/JAVA_CORE/section18_Interfaces/src/loan_service/entities/InterestService.java
@@ -1,12 +1,12 @@
 package loan_service.entities;
 
 public interface InterestService {
-    double getInterest();
+    double getInterestRate();
 
     default double payment(double amount, double month){
         if(month < 1){
             throw new IllegalArgumentException("Mont must be greater than 0 (zero)");
         }
-        return amount * Math.pow(1 + getInterest()/100,month);
+        return amount * Math.pow(1 + getInterestRate()/100,month);
     }
 }
