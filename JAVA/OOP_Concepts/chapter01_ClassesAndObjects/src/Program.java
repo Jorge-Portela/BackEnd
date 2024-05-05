2159d96ef845639afa2da4dc54e70a5ee3aa6610
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-04 23:53:02 -0300

    Added 'Program' class of 'Loan Service' mini project

--- JAVA/OOP_Concepts/chapter01_ClassesAndObjects/src/Program.java
+++ JAVA/OOP_Concepts/chapter01_ClassesAndObjects/src/Program.java
@@ -2,7 +2,7 @@ public class Program {
     public static void main(String[] args){
 
         Pen pen1 = new Pen();
-        pen1.model = "Bic Spherical";
+        pen1.model = "Mont Blanc";
         pen1.color = "Black";
         pen1.tip = 0.50;
         pen1.filler = 15;
