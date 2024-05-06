4df7e6f18d0a2ea10b2b6fbe551a50436a9194ed
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-06 02:19:12 -0300

    Adde the request of application

--- JAVA/JAVA_CORE/section19_GenericsSetMap/src/printer_system/application/Program.java
+++ JAVA/JAVA_CORE/section19_GenericsSetMap/src/printer_system/application/Program.java
@@ -5,6 +5,13 @@ import printer_system.entities.PrintService;
 import java.util.Locale;
 import java.util.Scanner;
 
+/*
+* Write a program that reads a quantity of N, and then N integers.
+* Finally, print these numbers in an organized way as per the example.
+* Then, inform the value entered.
+* */
+
+
 public class Program {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
