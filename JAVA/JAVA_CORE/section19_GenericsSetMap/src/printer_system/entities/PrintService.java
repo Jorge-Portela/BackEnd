b9482c98a4a8f0cfce3ac43b6a47ff9182c97b72
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-05 23:22:25 -0300

    Added 'Printer System' mini project and 'Print Sercie' class

---
+++ JAVA/JAVA_CORE/section19_GenericsSetMap/src/printer_system/entities/PrintService.java
@@ -0,0 +1,11 @@
+package printer_system.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService {
+
+    private List<Integer> list = new ArrayList<>();
+    
+
+}
