a893634f6d919e5183773bdb079bea267e350b9e
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-10 02:17:54 -0300

    Added 'printf' that print a value 10 in character type, that the same to use a '\n' in hexadecimals and ASCII

--- C_projects/practice_exam_linux/hello.c
+++ C_projects/practice_exam_linux/hello.c
@@ -4,6 +4,7 @@ int main(void){
   int ret = 0;
  
   printf("helloworld.com.br\n");
-  printf("%d - %d - %d -  %d\n",ret, 10, 0xa, 'A');
+  printf("%c",10);
+  printf("%x - %x - %x - %c\n",ret, 10, 0xa, 'A');
   return 0;
 }
