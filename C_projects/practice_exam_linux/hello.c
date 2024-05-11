e90cccc57ac50f202580d8deb766db4b1e5a8722
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-11 04:06:31 -0300

    Added a new parameter in 'orintf' function

--- C_projects/practice_exam_linux/hello.c
+++ C_projects/practice_exam_linux/hello.c
@@ -5,6 +5,6 @@ int main(void){
  
   printf("helloworld.com.br\n");
   printf("%c",10);
-  printf("%x - %x - %x - %c\n",ret, 10, 0xa, 0x41);
+  printf("%x - %x - %x - %c - %x\n",ret, 10, 0xa, 0x41,7);
   return 0;
 }
