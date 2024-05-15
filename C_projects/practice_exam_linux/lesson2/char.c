67c37694f51497fad02561ca24ecf3ae2e8bcee4
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-15 18:57:56 -0300

    Added a'printf' that print size of 'char' type

--- C_projects/practice_exam_linux/lesson2/char.c
+++ C_projects/practice_exam_linux/lesson2/char.c
@@ -2,10 +2,8 @@
 
 int main(void){
   char c = 255;
-  
-  printf("O tamanho de C (char): %lu bytes / %lu bits\n",sizeof c, sizeof c *8);  
-  c = 127;
-  printf("Valor de c: %i\n", c)
-    ;
+ 
+  printf("O tamanho do tipo char eh: %lu\n", sizeof (char)); 
+ 
   return 0;
 }
