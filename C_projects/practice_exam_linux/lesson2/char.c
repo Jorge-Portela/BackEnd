4c37c5b23bfad1ca7d1927fb0c54fdcb4ad67e8b
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-14 23:07:38 -0300

    Added print ype of c in 'bits' and 'bytes'

--- C_projects/practice_exam_linux/lesson2/char.c
+++ C_projects/practice_exam_linux/lesson2/char.c
@@ -3,7 +3,9 @@
 int main(void){
   char c = 255;
   
-  printf("O tamanho de C eh: %lu\n",sizeof c);  
-
+  printf("O tamanho de C (char): %lu bytes / %lu bits\n",sizeof c, sizeof c *8);  
+  c = 127;
+  printf("Valor de c: %i\n", c)
+    ;
   return 0;
 }
