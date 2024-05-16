392e7366560593c0560ae72d9c07e1ce7d4175de
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-16 04:44:07 -0300

    Added a program that print system size value of 'integer' type

--- C_projects/practice_exam_linux/lesson3/int.c
+++ C_projects/practice_exam_linux/lesson3/int.c
@@ -2,12 +2,10 @@
 #include <limits.h>
 
 int main(void){
-  int i = INT_MAX;  
- 
-  i++; // i = i + 1 ou i += 1
 
-  printf("The size of i (char): %zu bytes / %zu bits\n", sizeof i, sizeof i * 8); 
-  printf("Value of  i: %d\n",i);
+  short int it; 
+
+  printf("The value of int in this system is: '%lu'", sizeof it);
 
   return 0;
 }
