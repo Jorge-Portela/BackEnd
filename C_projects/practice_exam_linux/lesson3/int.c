46b6f6cd86b418174dd6446496e7be025085d688
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-16 05:07:33 -0300

    Added a 'UINT_MAX' oprator from 'stdint.h' library to use on print ize value of an int variable on terminal

--- C_projects/practice_exam_linux/lesson3/int.c
+++ C_projects/practice_exam_linux/lesson3/int.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <limits.h>
+#include <stdint.h>
 
 int main(void){
 
-  short int it; 
+  uint32_t i = UINT_MAX; 
 
-  printf("The value of int in this system is: '%lu'", sizeof it);
+  printf("The value of int in this system in bits: '%lu' / bytess: '%lu'", sizeof i, sizeof i * 8);
 
   return 0;
 }
