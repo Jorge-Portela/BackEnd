ba57914d141449da4f171a0d6d556e968e451399
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-16 02:37:02 -0300

    Added a modifier to 'i' variable of int type and print on terminal

--- C_projects/practice_exam_linux/lesson3/int.c
+++ C_projects/practice_exam_linux/lesson3/int.c
@@ -4,6 +4,8 @@
 int main(void){
   int i = INT_MAX;  
  
+  i++; // i = i + 1 ou i += 1
+
   printf("The size of i (char): %zu bytes / %zu bits\n", sizeof i, sizeof i * 8); 
   printf("Value of  i: %d\n",i);
 
