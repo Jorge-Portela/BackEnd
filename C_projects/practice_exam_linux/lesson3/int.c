d55569f7a3ae04850991c64b68b6996a6b39ef09
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-16 05:21:18 -0300

    Adde 'register' opeartor to alocate the 'i' variable onf 'int' type on rom CPU rather ram memory space. So this variable will processed fasther on compilator process

--- C_projects/practice_exam_linux/lesson3/int.c
+++ C_projects/practice_exam_linux/lesson3/int.c
@@ -4,9 +4,11 @@
 
 int main(void){
 
-  uint32_t i = UINT_MAX; 
+ register int i = 0; 
 
-  printf("The value of int in this system in bits: '%lu' / bytess: '%lu'", sizeof i, sizeof i * 8);
+  printf("The size value of int in this system in bits: %zu / bytes: %zu\n", sizeof i, sizeof i * 8);
+
+  printf("The value of i: %d\n",i);
 
   return 0;
 }
