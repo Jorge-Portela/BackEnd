01971c7fb201df85b32a84cc935bc2e7774834a5
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-18 05:57:27 -0300

    Added a program about size of float value concepts in C language

---
+++ C_projects/practice_exam_linux/lesson4/float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(void){
+  float f  = 1;
+
+  printf("The size of f (float) is: %zu bytes / %zu bits\n", sizeof f, sizeof f * 8);
+  printf("Value of f: %f\n",f);
+
+  return 0;
+}
