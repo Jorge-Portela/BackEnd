b209e262a18aaac6cff0ad2a3d7c67e38f9c6079
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-19 05:07:35 -0300

    Added a format with 3 decimals number on 'f' variable float type

--- C_projects/practice_exam_linux/lesson3/float.c
+++ C_projects/practice_exam_linux/lesson3/float.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int main(void){
-  float f = 3e2; // 3 * 10 ^ 2
+  float f = 3e3; // 3 * 10 ^ 2
 
   printf("The size of f(float) in bytes: %zu / bits %zu\n", sizeof f, sizeof f * 8);
   printf("The value of f: %.2f\n", f);
