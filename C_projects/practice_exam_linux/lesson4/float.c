1933127fdb469321f21cf484d3cf7955c9c3a960
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-20 23:02:07 -0300

    Added a new value to 'f' variable float type in scientific notation

--- C_projects/practice_exam_linux/lesson4/float.c
+++ C_projects/practice_exam_linux/lesson4/float.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 
 int main(void){
-  float f  = 1;
+  float f  = 1e4;
 
-  printf("The size of f (float) is: %zu bytes / %zu bits\n", sizeof f, sizeof f * 8);
+  printf("The size of f (float): %zu bytes / %zu bits\n", sizeof f, sizeof f * 8);
   printf("Value of f: %.2f\n",f);
 
   return 0;
