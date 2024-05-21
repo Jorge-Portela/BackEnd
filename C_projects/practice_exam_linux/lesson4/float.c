2a6880430af33164ef0817a4b8199e68dbad20e8
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-05-21 01:10:08 -0300

    Added 'd' variable double type with longer modifier

--- C_projects/practice_exam_linux/lesson4/float.c
+++ C_projects/practice_exam_linux/lesson4/float.c
@@ -5,12 +5,13 @@
 
 int main(void){
   float f  = 1;
-  double d = 1;
+  long double d = 1;
 
   printf("The size of f (float): %zu bytes / %zu bits\n", sizeof f, sizeof f * 8);
   printf("The size of d (double): %zu bytes / %zu bits\n", sizeof d, sizeof d*8);
+  printf("The size of d (long double): %zu bytes / %zu bits\n", sizeof d, sizeof d*8);
   printf("Value of f: %.2f\n",f);
-  printf("Value of d: %.2f\n",d);
+  printf("Value of d: %.2Lf\n",d);
 
   return 0;
 }
